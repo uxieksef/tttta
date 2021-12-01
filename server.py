60f245f5041a284c65768e190bff5456cec4e5aa
uxieksef <56426949+uxieksef@users.noreply.github.com>
2021-12-01 15:09:34 +0800

    up3

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ PORT = 8080
 name = os.environ['NAME']
 if name == None or len(name) == 0:
   name = "world"
-MESSAGE = "auto compile, see please, " + name + "!"
+MESSAGE = "auto compile second time, can you see please, " + name + "!"
 print("Message: '" + MESSAGE + "'")
 
 app = Flask(__name__)
